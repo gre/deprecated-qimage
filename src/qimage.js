d8be0c041b4eff891fb0533a736c0aebf7edb6e5
Gaëtan Renaudeau <gre@zenexity.com>
2013-06-23 20:18:46 +0200

    Init version 1.0.0

---
+++ src/qimage.js
@@ -0,0 +1,36 @@
+/*
+ * Qimage - Simple Promise Image Loader based on Q
+ */
+(function () {
+
+  // QImage
+  // ===
+  // *Creates an Image Loader.*
+  //
+  // Parameters
+  // ---
+  // `url` **(string)**: load an Image with its URL
+  //
+  // Result
+  // ---
+  // Returns a **(Promise of Image)**
+  //
+  window.Qimage = function (url) {
+    var img = new Image();
+    var d = Q.defer();
+    img.onload = function () {
+      d.resolve(img);
+    };
+    img.onabort = function (e) {
+      d.reject(e);
+    };
+    img.onerror = function (e) {
+      d.reject(e);
+    };
+    img.src = url;
+    return d.promise;
+  };
+
+})();
+
+
